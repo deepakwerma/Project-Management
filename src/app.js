8ad5522371cb60f41f80272630d0b47ab1ca01fb
Deepak verma <deepakrvermaa@gmail.com>
2025-11-28 08:27:38 +0530

    Register User and the mail service successfully

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5713",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+// import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to basecampy");
+});
+
+export default app;
